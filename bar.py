71bc2656214e653d1b2bcbe6358213f8e8ba509c
Juhnowski <juhnowski@gmail.com>
2019-02-07 04:45:32 +0300

    my github commits visualisation

--- bar.py
+++ bar.py
@@ -1,13 +1,15 @@
 from matplotlib import pyplot as plt
+import numpy as np
 
-movies = ["Энни Холл","Бен-Гур","Касабланка","Ганди","Вестсайдская история"]
-num_oscars = [5,11,3,8,10]
+movies = ["Witt-java-1","py_ds_simple_visualisations","Witt-java","eflute"]
+num_commits = [4,3,2,1]
 
 xs = [i+0.1 for i,_ in enumerate(movies)]
 
-plt.bar(xs,num_oscars)
-plt.ylabel("Количество наград")
-plt.title("Мои любимые фильмы")
+plt.bar(xs,num_commits)
+plt.ylabel("Commits")
+plt.yticks(np.arange(min(num_commits), max(num_commits)+1, 1.0))
+plt.title("My GitHub February commits")
 
-plt.xticks([i + 0.5 for i, _ in enumerate(movies)], movies)
+plt.xticks([i + 0.1 for i, _ in enumerate(movies)], movies)
 plt.show()
